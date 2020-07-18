d702f2c92b4150f7bd2a6128bb0ae50cdb47f763
Audry Wolters <audrywolters@gmail.com>
2020-07-18 17:17:11 -0500

    this is big commit. pretty much everything. I was trying to figure out routes and got distracted. works tho

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,8 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 // import axios from 'axios';
 import Header from '../Header/Header';
 import Feeling from '../Feeling/Feeling';
+import Understand from '../Understand/Understand';
+import Support from '../Support/Support';
 
 class App extends Component {
   render() {
@@ -12,6 +14,8 @@ class App extends Component {
 
         <Router>
           <Route exact path="/" component={ Feeling } />
+          <Route path="/understand" component={ Understand } />
+          <Route path="/support" component={ Support } />
         </Router>
 
       </>
