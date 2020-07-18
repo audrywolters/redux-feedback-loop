9d5ee40ce66210f1202633580bf5d178b95e7b82
Audry Wolters <audrywolters@gmail.com>
2020-07-18 13:10:46 -0500

    installed router. created exact route for Feeling.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { HashRouter as Router, Route } from 'react-router-dom';
+// import axios from 'axios';
 import Header from '../Header/Header';
 import Feeling from '../Feeling/Feeling';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <>
         <Header />
-        <Feeling />
+
+        <Router>
+          <Route exact path="/" component={ Feeling } />
+        </Router>
+
       </>
     );
   }
