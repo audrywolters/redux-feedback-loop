cc980971f9cc1ce4865978803198fd55a098def0
Audry Wolters <audrywolters@gmail.com>
2020-07-18 15:00:56 -0500

    changed to radio buttons. something else.

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -2,21 +2,47 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Feeling extends Component {
-    
-    onFeelingChange = () => {
-        // get feeling input value
-        console.log( this.props );
+
+    onFeelingChange = ( event ) => {
         // send feeling value to redux pot
         this.props.dispatch( { type: 'SET_FEELING', 
-                               payload: { feeling: this.props.feeling } } );
+                               payload: { feeling: event.target.value } } );
     }
 
+    
     render() {
 
         return (
             <>
-                <label>How are you feeling today?</label>
-                <input onChange={ this.onFeelingChange } type="number" min="0" max="5" value="0" />
+                <label>How are you feeling today?</label><br />
+
+                {/* ONE */}
+                <input onChange={ this.onFeelingChange } 
+                       type="radio" id="one" name="feeling" value="1" checked={ true } />
+                <label htmlFor="one">1</label>
+
+                {/* TWO */}
+                <input onChange={ this.onFeelingChange } 
+                       type="radio" id="two" name="feeling" value="2" />
+                <label htmlFor="two">2</label>
+
+                {/* THREE */}
+                <input onChange={ this.onFeelingChange } 
+                       type="radio" id="three" name="feeling" value="3" />
+                <label htmlFor="three">3</label>
+
+                {/* FOUR */}
+                <input onChange={ this.onFeelingChange } 
+                       type="radio" id="four" name="feeling" value="4" />
+                <label htmlFor="four">4</label>
+
+                {/* FIVE */}
+                <input onChange={ this.onFeelingChange } 
+                       type="radio" id="five" name="feeling" value="5" />
+                <label htmlFor="five">5</label>
+                
+                <br />
+
                 <button>Next</button>
             </>
         );
--- src/index.js
+++ src/index.js
@@ -3,24 +3,21 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
-
 // AUDRY - what is this and do I need it. we did fine w/o it in pizza parlor
 import registerServiceWorker from './registerServiceWorker';
-
+//
 import App from './components/App/App';
 import './index.css';
 
+
 // catch the user's feeling of the day
-const setFeeling = ( state=[], action ) => {
-    // AUDRY - remove
-    console.log( 'payload', action.payload );
+const setFeeling = ( state={}, action ) => {
 
     if ( action.type === 'SET_FEELING' ) {
         return action.payload;
     }
     return state;
 }
-
 
 
 
@@ -33,10 +30,6 @@ const storeInstance = createStore(
     // lets us use the v nice console display of all our data
     applyMiddleware( logger )
 )
-
-
-
-
 
 // render the stuff, lovingly cradled by redux
 ReactDOM.render( <Provider store={ storeInstance }><App /></Provider>,
